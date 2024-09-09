b3d4baf62569202c32230a0c73a0838884677d81
davidrevo <davidrevo.wdai7@gmail.com>
2024-09-10 00:38:24 +0800

    Function, DataLocations

--- contracts/DataLocations.sol
+++ contracts/DataLocations.sol
@@ -11,13 +11,15 @@ contract DataLocations {
 
     mapping(uint256 => MyStruct) myStructs;
 
-    function f() public {
+    function f() public returns (MyStruct memory) {
         // call _f with state variables
         _f(arr, map, myStructs[1]);
         // get a struct from a mapping
         MyStruct storage myStruct = myStructs[1];
-        // craete a struct in memory
+        // create a struct in memory
         MyStruct memory myMemStruct = MyStruct(0);
+
+        return myMemStruct;
     }
 
     function _f(
@@ -26,5 +28,14 @@ contract DataLocations {
         MyStruct storage _myStruct
     ) internal {
         // do something with storage variables
+    }
+
+    // You can return memory variables
+    function g(uint256[] memory _arr) public returns (uint256[] memory) {
+        // do something with memory array
+    }
+
+    function h(uint256[] calldata _arr) external {
+        // do something with calldata array
     }
 }
