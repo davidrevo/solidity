f10a9d4c39c56dbb7129b596f9fe283ae1c88cd7
davidrevo <davidrevo.wdai7@gmail.com>
2024-09-09 20:20:42 +0800

    DataLocations

---
+++ contracts/DataLocations.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract DataLocations {
+    uint256[] public arr;
+    mapping(uint256 => address) map;
+
+    struct MyStruct {
+        uint256 foo;
+    }
+
+    mapping(uint256 => MyStruct) myStructs;
+
+    function f() public {
+        // call _f with state variables
+        _f(arr, map, myStructs[1]);
+        // get a struct from a mapping
+        MyStruct storage myStruct = myStructs[1];
+        // craete a struct in memory
+        MyStruct memory myMemStruct = MyStruct(0);
+    }
+
+    function _f(
+        uint256[] storage _arr,
+        mapping(uint256 => address) storage _map,
+        MyStruct storage _myStruct
+    ) internal {
+        // do something with storage variables
+    }
+}
